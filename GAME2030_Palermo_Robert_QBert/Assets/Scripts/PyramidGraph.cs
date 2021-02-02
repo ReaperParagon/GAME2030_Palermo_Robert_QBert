e7ecd3e1454fad1f0f47f4a343f977bca31b7c2e
Robert Palermo <robert.palermo.official@gmail.com>
2021-02-02 16:02:21 -0500

    Added Pyramid Nodes (Unconnected)

---
+++ GAME2030_Palermo_Robert_QBert/Assets/Scripts/PyramidGraph.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidGraph : MonoBehaviour
+{
+    public PyramidNode[] Nodes = new PyramidNode[28];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assign sprites to each Node, set them all to unactivated
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GAME2030_Palermo_Robert_QBert/Assets/Scripts/PyramidNode.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class PyramidNode : MonoBehaviour
+{
+    public bool bActivated = false;
+    public Transform m_transform;
+}
